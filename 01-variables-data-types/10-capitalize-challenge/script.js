f3c9526350fe59e02cf9c92ae2d13d3496751b09
Nam Le <namle94.dev@gmail.com>
2023-07-26 15:39:25 -0400

    solves first challenge

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,13 +1,18 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the lowercase value from "myString"
 const myString = 'developer';
 
-let myNewString;
+let newString;
 
-// Solution 1:
-myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
-// Solution 2:
-myNewString = myString[0].toUpperCase() + myString.substring(1);
-// Solution 3:
-myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+newString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+console.log(newString);
 
-console.log(myNewString);
+// let myNewString;
+
+// // Solution 1:
+// myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+// // Solution 2:
+// myNewString = myString[0].toUpperCase() + myString.substring(1);
+// // Solution 3:
+// myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+
+// console.log(myNewString);
