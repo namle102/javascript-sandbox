67ab59bf500b714ffe5b479ac2b3b0a0177d68e5
Nam Le <namle94.dev@gmail.com>
2023-07-26 16:35:58 -0400

    completes array challenge

--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -1,28 +1,44 @@
-// Challenge 1
-
-const arr = [1, 2, 3, 4, 5];
-
-// arr.reverse();
-// arr.push(0);
-// arr.unshift(6);
-
-// Same result as above
+const arr = [1,2,3,4,5];
 arr.push(6);
 arr.unshift(0);
 arr.reverse();
+// console.log(arr);
 
-console.log(arr);
+const arr1 = [1,2,3,4,5];
+const arr2 = [5,6,7,8,9,10];
+let arr3;
 
-// Challenge 2
+if (arr1[arr1.length - 1] === arr2[0]) {
+    arr3 = [...arr1, ...arr2.slice(1)];
+}
+console.log(arr3);
 
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [5, 6, 7, 8, 9, 10];
 
-// Solution 1
-const arr3 = arr1.slice(0, 4).concat(arr2);
+// // Challenge 1
 
-// Solution 2
-const arr4 = [...arr1, ...arr2];
-arr4.splice(4, 1);
+// const arr = [1, 2, 3, 4, 5];
 
-console.log(arr4);
+// // arr.reverse();
+// // arr.push(0);
+// // arr.unshift(6);
+
+// // Same result as above
+// arr.push(6);
+// arr.unshift(0);
+// arr.reverse();
+
+// console.log(arr);
+
+// // Challenge 2
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [5, 6, 7, 8, 9, 10];
+
+// // Solution 1
+// const arr3 = arr1.slice(0, 4).concat(arr2);
+
+// // Solution 2
+// const arr4 = [...arr1, ...arr2];
+// arr4.splice(4, 1);
+
+// console.log(arr4);
